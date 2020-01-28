966035239959c561357cc03c9f719e2fd8d2028c
Taras-B <taras5475@gmail.com>
2020-01-28 15:26:46 +0200

    Updates

---
+++ precache-manifest.41955dfdcd95e71e574785be2ce5d243.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af07baf9b7aac82ed71bc6f8cb14a455",
+    "url": "/my-social-react-project/index.html"
+  },
+  {
+    "revision": "ade19f06f86a04c39ff4",
+    "url": "/my-social-react-project/static/css/3.e483a630.chunk.css"
+  },
+  {
+    "revision": "8731733f255bde7435c8",
+    "url": "/my-social-react-project/static/css/4.e062972e.chunk.css"
+  },
+  {
+    "revision": "9b690fd0480860eb5905",
+    "url": "/my-social-react-project/static/css/main.b82bbcac.chunk.css"
+  },
+  {
+    "revision": "f04f36166bed09839330",
+    "url": "/my-social-react-project/static/js/2.a03e8f0d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/my-social-react-project/static/js/2.a03e8f0d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ade19f06f86a04c39ff4",
+    "url": "/my-social-react-project/static/js/3.6795772c.chunk.js"
+  },
+  {
+    "revision": "8731733f255bde7435c8",
+    "url": "/my-social-react-project/static/js/4.85019087.chunk.js"
+  },
+  {
+    "revision": "9b690fd0480860eb5905",
+    "url": "/my-social-react-project/static/js/main.eabc9c36.chunk.js"
+  },
+  {
+    "revision": "162757418926b004a110",
+    "url": "/my-social-react-project/static/js/runtime-main.17dae539.js"
+  },
+  {
+    "revision": "435a9b856fd54cbd5a38126cb553a092",
+    "url": "/my-social-react-project/static/media/loading.435a9b85.svg"
+  }
+]);
